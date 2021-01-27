694f145ae98a43cda5982c4818e11240f9445851
acasadiegos <acasadiego199827@gmail.com>
2021-01-27 15:49:38 -0500

    Subida de proyecto blog de videojuegos

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por Andrés Casadiegos &copy; <?=date('Y');?></p>
+		</footer>
+		
+	</body>
+</html>
